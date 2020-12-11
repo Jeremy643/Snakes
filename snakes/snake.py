84b14502c2b87f049747a16c4e4f85237a0d8f25
JeremyScott17 <jeremyascott17@gmail.com>
2020-12-11 22:42:57 +0000

    Implement new features for Snake instances
    * Implement __str__ and __repr__
    * Initialize a speed and direction for the snake
    * Implement a method to change the direction of the snake based
      on user input

--- snakes/snake.py
+++ snakes/snake.py
@@ -8,11 +8,28 @@ class Snake:
         self.win = win
         self.start_pos = (WIDTH//2, HEIGHT//2)
         self.body = deque([self.start_pos])
+
+        self.speed = START_SPEED
+        self.direction = RIGHT
+    
+    def __str__(self):
+        return f'SNAKE: length = {len(self.body)} | speed = {self.speed} | direction = {self.direction}'
+    
+    def __repr__(self):
+        return f'{__class__.__name__}({repr(self.win)})'
     
     def _draw_snake(self):
         for seg in self.body:
             x, y = seg
             pygame.draw.rect(self.win, BROWN, (x, y, BODY_WIDTH, BODY_HEIGHT))
+    
+    def move(self):
+        """Increase the position of each body segment with respect to the direction."""
+        pass
+
+    def change_direction(self, snake_dir):
+        """Change the direction of the snake based on the user's input."""
+        self.direction = snake_dir
 
     def update(self):
         self._draw_snake()

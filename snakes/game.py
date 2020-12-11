a6e39b831b22be611ced5ecc4c1ea75ac15dfe4e
JeremyScott17 <jeremyascott17@gmail.com>
2020-12-11 22:39:33 +0000

    Implement __str__ and __repr__ methods to describe the game class
    * Initialize a fruit object when a new game instance is created

--- snakes/game.py
+++ snakes/game.py
@@ -1,19 +1,32 @@
 import pygame
 from .constants import *
 from .snake import Snake
+from .fruit import Fruit
 
 class Game:
 
     def __init__(self, win):
         self.win = win
         self.snake = Snake(win)
+        self.fruit = Fruit(win)
+    
+    def __str__(self):
+        return f'{str(self.snake)}\n{str(self.fruit)}'
+
+    def __repr__(self):
+        return f'{__class__.__name__}({repr(self.win)})'
 
     def _draw_window(self):
         # draw the background
         pygame.draw.rect(self.win, GREEN, (0, 0, WIDTH, HEIGHT))
 
-        #draw the snake
+        # draw the snake and fruit
         self.snake.update()
+        self.fruit.update()
+    
+    def user_input(self, snake_dir):
+        """Handles the user's input."""
+        self.snake.change_direction(snake_dir)
     
     def update(self):
         """Draw background, snake and fruit then update pygame window."""

0f91a32bf0844fb414df78dd9d39b5ba93dfbe59
JeremyScott17 <jeremyascott17@gmail.com>
2020-12-11 17:58:12 +0000

    Implement functionality for the snake object
    * Display and update the starting length and position of the snake
      on the window
    * Update the constant variables for the snake object

--- snakes/constants.py
+++ snakes/constants.py
@@ -4,5 +4,9 @@ import pygame
 # window
 WIDTH, HEIGHT = 800, 800
 
-# background colour - rgb
+# snake
+BODY_WIDTH, BODY_HEIGHT = 25, 25
+
+# rgb colours
 GREEN = (17, 196, 17)
+BROWN = (102, 51, 0)
--- snakes/snake.py
+++ snakes/snake.py
@@ -1,9 +1,18 @@
 import pygame
+from .constants import *
+from collections import deque
 
 class Snake:
 
-    def __init__(self):
-        pass
+    def __init__(self, win):
+        self.win = win
+        self.start_pos = (WIDTH//2, HEIGHT//2)
+        self.body = deque([self.start_pos])
+    
+    def _draw_snake(self):
+        for seg in self.body:
+            x, y = seg
+            pygame.draw.rect(self.win, BROWN, (x, y, BODY_WIDTH, BODY_HEIGHT))
 
     def update(self):
-        pass
+        self._draw_snake()

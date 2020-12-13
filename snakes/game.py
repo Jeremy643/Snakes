8da1b14b3119d6e3e1cd65782ad4122ece2404bb
JeremyScott17 <jeremyascott17@gmail.com>
2020-12-13 20:20:55 +0000

    Implement a check on if the snake eats the fruit

--- snakes/game.py
+++ snakes/game.py
@@ -24,6 +24,23 @@ class Game:
         self.snake.update()
         self.fruit.update()
     
+    def _fruit_eaten(self):
+        x_head, y_head = self.snake.get_head()
+        x_fruit, y_fruit = self.fruit.position
+
+        x_snake_cov = set(range(x_head, x_head + BODY_WIDTH + 1))
+        y_snake_cov = set(range(y_head, y_head + BODY_HEIGHT + 1))
+
+        x_fruit_cov = set(range(x_fruit, x_fruit + FRUIT_WIDTH + 1))
+        y_fruit_cov = set(range(y_fruit, y_fruit + FRUIT_HEIGHT + 1))
+
+        if (x_snake_cov & x_fruit_cov) and (y_snake_cov & y_fruit_cov):
+            self.snake.grow()
+            self.fruit.eat()
+            return True
+        
+        return False
+            
     def user_input(self, snake_dir):
         """Handles the user's input."""
         self.snake.change_direction(snake_dir)
@@ -31,4 +48,8 @@ class Game:
     def update(self):
         """Draw background, snake and fruit then update pygame window."""
         self._draw_window()
+
+        # check if snake eats fruit
+        self._fruit_eaten()
+
         pygame.display.update()

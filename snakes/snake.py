4a0749441acec79bbe1d588bd61e005368d7e08a
JeremyScott17 <jeremyascott17@gmail.com>
2020-12-13 20:27:07 +0000

    Implement snake movement and the snake eating the fruit
    * The entire snake body moves but the body doesn't follow the head
      around corners correctly
    * Implement the snake to grow when it eats some fruit

--- snakes/snake.py
+++ snakes/snake.py
@@ -6,8 +6,10 @@ class Snake:
 
     def __init__(self, win):
         self.win = win
+
         self.start_pos = (WIDTH//2, HEIGHT//2)
         self.body = deque([self.start_pos])
+        self.path = deque()
 
         self.speed = START_SPEED
         self.direction = RIGHT
@@ -24,12 +26,46 @@ class Snake:
             x, y = seg
             pygame.draw.rect(self.win, BROWN, (x, y, BODY_WIDTH, BODY_HEIGHT))
     
+    def _update_path(self):
+        self.path.appendleft(self.body[0])
+        while len(self.path) > len(self.body) - 1:
+            self.path.pop()
+    
+    def get_head(self):
+        return self.body[0]
+    
+    def grow(self):
+        x_last, y_last = self.body[-1]
+
+        if self.direction == RIGHT or self.direction == LEFT:
+            pos = (x_last - (self.direction[0] * len(self.body) * BODY_WIDTH), y_last)
+        else:
+            pos = (x_last, y_last - (self.direction[1] * len(self.body) * BODY_HEIGHT))
+        
+        self.body.append(pos)
+
     def move(self):
         """Increase the position of each body segment with respect to the direction."""
+        self._update_path()
+
         x_vel, y_vel = tuple(map(lambda d: d * self.speed, self.direction))
-        for i in range(len(self.body)):
-            x_body, y_body = self.body[i]
-            self.body[i] = (x_vel + x_body, y_vel + y_body)
+        x_head, y_head = self.body[0]
+        self.body[0] = (x_vel + x_head, y_vel + y_head)
+
+        for i in range(1, len(self.body)):
+            path_index = i - 1
+
+            x_path, y_path = self.path[path_index]
+            if self.direction == RIGHT or self.direction == LEFT:
+                self.body[i] = (x_path - (self.direction[0] * i * BODY_WIDTH), y_path)
+            else:
+                self.body[i] = (x_path, y_path - (self.direction[1] * i * BODY_HEIGHT))
+
+            path_index += 1
+
+        # for i in range(len(self.body)):
+        #     x_body, y_body = self.body[i]
+        #     self.body[i] = (x_vel + x_body, y_vel + y_body)
 
     def change_direction(self, snake_dir):
         """Change the direction of the snake based on the user's input."""
